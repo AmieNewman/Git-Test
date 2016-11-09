dfdfba015a2852b44b17222f0626e0ef3c3a5983
Amie Newman <amilnewm@ut.utm.edu>
2016-11-08 20:19:46 -0600

    Revised and edited main

--- main.cpp
+++ main.cpp
@@ -3,8 +3,28 @@
 //main.cpp
 //Simple Hello-world file
 
+using namespace std;
+
 #include <iostream>
 
+int sum(int n);
+
 int main(){
-  std::cout << "Hello World" << std::endl;
+  cout << "Hello World" << endl;
+  cout << "Give me a number" << endl;
+  int n=0;
+  cin >> n ;
+  cout << sum(n) << endl;
+  cout << "Done with program" << endl;
+  
+  
+
+}
+int sum(int n){
+  int j=0;
+  for(int i=0; i <= n ; i++){
+    j += i;
+  }
+
+  return j;
 }
